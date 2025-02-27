342eaf97192fcf5f262856d6ec526830b3b29ead
rafif <rafifidnsolo007@gmail.com>
2025-02-27 09:18:59 +0700

    first commit

---
+++ bookish-master/assets/js/script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnelem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnelem(navToggler, 'click', toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnelem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active on scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnelem(window, "scroll", activeHeader);
+
+
+
+/**
+ * filter tab
+ */
+
+const tabCard = document.querySelectorAll("[data-tab-card]");
+
+let lastTabCard = tabCard[0];
+
+const navigateTab = function () {
+  lastTabCard.classList.remove("active");
+  this.classList.add("active");
+  lastTabCard = this;
+}
+
+addEventOnelem(tabCard, "click", navigateTab);
+
+// Ambil elemen tombol "Read more" dan pop-up
+const readMoreBtn = document.getElementById("read-more");
+const popup = document.getElementById("popup");
+const closePopupBtn = document.getElementById("close-popup");
+
+// Event untuk menampilkan pop-up
+readMoreBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // Mencegah aksi default (navigasi link)
+  popup.style.display = "flex"; // Tampilkan pop-up
+});
+
+// Event untuk menutup pop-up
+closePopupBtn.addEventListener("click", function () {
+  popup.style.display = "none"; // Sembunyikan pop-up
+});
